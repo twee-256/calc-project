e9dd002952d970aacde66efb94f1771ec47f4524
Jesutooni Oguntula <joguntula@gmail.com>
2025-10-24 18:47:15 -0500

    added calc layout

--- scripts.js
+++ scripts.js
@@ -14,7 +14,29 @@ function divide(a, b) {
     return Math.round((a / b) * 10000) / 10000;
 }
 
-console.log(add(3, 6));
-console.log(subtract(13, 6));
-console.log(multiply(13, 6));
-console.log(divide(13, 6));
+function operate(operator, a, b) {
+    // operator = '';
+    switch (operator) {
+        case '+':
+            add(a, b);
+            console.log(add(a, b));
+            break;
+
+        case '-':
+            subtract(a, b);
+            console.log(subtract(a, b))
+            break;
+
+        case '*':
+            multiply(a, b);
+            console.log(multiply(a, b))
+            break;
+
+        case '/':
+            divide(a, b);
+            console.log(divide(a, b))
+            break;
+    }
+}
+
+let display = document.querySelector('.display');
