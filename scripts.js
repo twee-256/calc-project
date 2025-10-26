0c40c545eab584a911d618033c7e7169c4d1cc7c
Jesutooni Oguntula <joguntula@gmail.com>
2025-10-26 16:19:19 -0500

    added styling and keyboard support for number and signs

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return Math.round((a / b) * 10000) / 10000;
+    return a / b;
 }
 
 function operate(operator, a, b) {
@@ -43,75 +43,93 @@ let operand = '';
 let result = '';
 let signs = ['+', '-', '×', '÷'];
 
-function calc() {
-    let digits = document.querySelectorAll('.digit');
+let digits = document.querySelectorAll('.digit');
 
-    digits.forEach(digit => {
-        digit.addEventListener('click', event => {
-            const value = digit.value;
-            let expr = display.textContent;
-            if (value !== '=') {
-                if (!signs.includes(value)) {
-                    if (display.textContent == '0') {
-                        display.textContent = '';
-                    }
-                    if (value != '.') {
-                        display.textContent += value;
-                    }
-                }
+digits.forEach(digit => {
+    digit.addEventListener('click', event => {
+        const value = digit.value;
+        calc(value);
+    });
+});
+
+document.addEventListener("keydown", event => {
+    let key = event.key;
+    if (/\d/.test(key) || /[+-.]/.test(key)) {
+        calc(key);
+    }
+
+    const keyMap = {
+        '*': '×',
+        '/': '÷',
+    };
+    const mapped = keyMap[key];
+    if (mapped) {
+        calc(mapped);
+    }
+})
+
+function calc(value) {
+    let expr = display.textContent;
+    if (value != '=') {
+        if (!signs.includes(value)) {
+            if (display.textContent == '0') {
+                display.textContent = '';
             }
-            if (value == '.' && !expr.includes(value)) {
-                if (signs.includes(expr.slice(-1))) {
-                    console.log(expr.slice(-1));
-                    display.textContent += '0';
-                    display.textContent += value;
-                } else {
-                    display.textContent += value;
-                }
-                // console.log(expr.slice(-1));
+            if (value != '.') {
+                display.textContent += value;
             }
-            if (value === '=') {
-                for (let sign of signs) {
-                    if (expr.includes(sign)) {
-                        const array = expr.split(sign);
-                        num1 = array[0];
-                        num2 = array[1];
-                        if (num1 != '' && num2 != '') {
-                            result = operate(sign, num1, num2);
-                            display.textContent = result;
-                        }
-                        return;
-                    }
-                }
-            } else if (signs.includes(value)) {
-                if (expr.includes(value)) {
-                    const array = expr.split(value);
-                    num1 = array[0];
-                    num2 = array[1];
-                    if (num1 != '' && num2 != '') {
-                        result = operate(value, num1, num2);
-                        display.textContent = result + value;
-                    }
-                }
-                if (!expr.includes(value)) {
-                    for (let sign of signs) {
-                        if (expr.includes(sign)) {
-                            const array = expr.split(sign);
-                            num1 = array[0];
-                            num2 = array[1];
-                            if (num1 != '' && num2 != '') {
-                                result = operate(sign, num1, num2);
-                                display.textContent = result + value;
-                            }
-                            return;
-                        }
-                    }
-                    display.textContent += value;
+        }
+    }
+    if (value == '.' && expr.slice(-1) != value) {
+        if (signs.includes(expr.slice(-1))) {
+            console.log(expr.slice(-1));
+            display.textContent += '0';
+            display.textContent += value;
+        } else {
+            display.textContent += value;
+        }
+        // console.log(expr.slice(-1));
+    }
+    if (value == '=') {
+        for (let sign of signs) {
+            if (expr.includes(sign)) {
+                const array = expr.split(sign);
+                num1 = array[0];
+                num2 = array[1];
+                if (num1 != '' && num2 != '') {
+                    result = Math.round(operate(sign, num1, num2) * 10000) / 10000;
+                    display.textContent = result;
                 }
                 return;
             }
-        });
-    });
+        }
+    } else if (signs.includes(value)) {
+        if (expr.includes(value)) {
+            const array = expr.split(value);
+            num1 = array[0];
+            num2 = array[1];
+            if (num1 != '' && num2 != '') {
+                result = operate(value, num1, num2);
+                display.textContent = result + value;
+            }
+        }
+        if (!expr.includes(value)) {
+            for (let sign of signs) {
+                if (expr.includes(sign)) {
+                    const array = expr.split(sign);
+                    num1 = array[0];
+                    num2 = array[1];
+                    if (num1 != '' && num2 != '') {
+                        result = Math.round(operate(sign, num1, num2) * 10000) / 10000;
+                        display.textContent = result + value;
+                    }
+                    return;
+                }
+            }
+            display.textContent += value;
+        }
+        return;
+    }
 }
 
 function backspace() {
@@ -135,5 +153,22 @@ function clear() {
 }
 
 backspace();
-calc();
+// document.addEventListener('keydown', (e) => {
+//     console.log('Key is down:', e.key);
+// });
+// let digits = document.querySelectorAll('.digit');
+// digits.forEach(digit => {
+//     document.addEventListener("keydown", event => {
+//         if (/\d/.test(event.key) == digit) {
+//             console.log('Numeber is:', digit);
+//         }
+//     })
+
+// })
+// document.addEventListener("keydown", event => {
+//     if (/\d/.test(event.key)) {
+//         console.log('Numeber is:', event.key);
+//     }
+// })
+// calc();
 clear();
