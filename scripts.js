59010688298a251ef53bcf0b4b4cfbd67199e5f5
Jesutooni Oguntula <joguntula@gmail.com>
2025-10-25 19:59:40 -0500

    edited calculation functionality

--- scripts.js
+++ scripts.js
@@ -24,10 +24,13 @@ function operate(operator, a, b) {
         case '-':
             return subtract(a, b);
 
-        case '*':
+        case '×':
             return multiply(a, b);
 
-        case '/':
+        case '÷':
+            if (b == '0') {
+                return 'Really???'
+            }
             return divide(a, b);
 
     }
@@ -38,7 +41,7 @@ let num1 = '';
 let num2 = '';
 let operand = '';
 let result = '';
-let signs = ['+', '-', '*', '/'];
+let signs = ['+', '-', '×', '÷'];
 
 function calc() {
     let digits = document.querySelectorAll('.digit');
@@ -48,10 +51,24 @@ function calc() {
             const value = digit.value;
             let expr = display.textContent;
             if (value !== '=') {
-                if (display.textContent == '0') {
-                    display.textContent = '';
+                if (!signs.includes(value)) {
+                    if (display.textContent == '0') {
+                        display.textContent = '';
+                    }
+                    if (value != '.') {
+                        display.textContent += value;
+                    }
                 }
-                display.textContent += value;
+            }
+            if (value == '.' && !expr.includes(value)) {
+                if (signs.includes(expr.slice(-1))) {
+                    console.log(expr.slice(-1));
+                    display.textContent += '0';
+                    display.textContent += value;
+                } else {
+                    display.textContent += value;
+                }
+                // console.log(expr.slice(-1));
             }
             if (value === '=') {
                 for (let sign of signs) {
@@ -67,18 +84,31 @@ function calc() {
                     }
                 }
             } else if (signs.includes(value)) {
-                for (let sign of signs) {
-                    if (expr.includes(sign)) {
-                        const array = expr.split(sign);
-                        num1 = array[0];
-                        num2 = array[1];
-                        if (num1 != '' && num2 != '') {
-                            result = operate(sign, num1, num2);
-                            display.textContent = result + value;
-                        }
-                        return;
+                if (expr.includes(value)) {
+                    const array = expr.split(value);
+                    num1 = array[0];
+                    num2 = array[1];
+                    if (num1 != '' && num2 != '') {
+                        result = operate(value, num1, num2);
+                        display.textContent = result + value;
                     }
                 }
+                if (!expr.includes(value)) {
+                    for (let sign of signs) {
+                        if (expr.includes(sign)) {
+                            const array = expr.split(sign);
+                            num1 = array[0];
+                            num2 = array[1];
+                            if (num1 != '' && num2 != '') {
+                                result = operate(sign, num1, num2);
+                                display.textContent = result + value;
+                            }
+                            return;
+                        }
+                    }
+                    display.textContent += value;
+                }
+                return;
             }
         });
     });
@@ -88,6 +118,9 @@ function backspace() {
     let bkSpace = document.querySelector('.bkSpace');
     bkSpace.addEventListener('click', event => {
         display.textContent = display.textContent.slice(0, -1);
+        if (display.textContent === '') {
+            display.textContent = '0';
+        }
     })
 }
 
@@ -95,6 +128,9 @@ function clear() {
     let clear = document.querySelector('.clear');
     clear.addEventListener('click', event => {
         display.textContent = '';
+        if (display.textContent == '') {
+            display.textContent = '0';
+        }
     })
 }
 
